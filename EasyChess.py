56c267965677a34135e4e4ccb38f3d827c480a45
Bruce Shiundu <epx2yd@inf.elte.hu>
2021-11-11 16:59:56 +0100

    Set up automatic github release whenever there is a commit on main branch

---
+++ EasyChess.py
@@ -0,0 +1,17 @@
+# We add all the files in the app directory so that they can be found
+
+# from sys import path
+# path.append('..\\app\\views')
+
+from app.views.BoardGuiTk import BoardGuiTk
+# from BoardGuiTk import BoardGuiTk
+
+if __name__ == "__main__":
+
+    boardGUI = BoardGuiTk()
+
+    boardGUI.drawBoard()
+
+    boardGUI.draw_pieces()
+
+    boardGUI.mainloop()

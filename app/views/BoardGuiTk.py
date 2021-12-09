e73afa66c0bf50dcb090f94e4b8f9c378f954176
Dion Kajdomcaj <dion.kajdomcaj@outlook.com>
2021-12-09 17:35:56 +0100

    fixing CI errors

--- app/views/BoardGuiTk.py
+++ app/views/BoardGuiTk.py
@@ -83,10 +83,10 @@ class BoardGuiTk(tk.Tk):
     def popupmsg(msg, title):
         root = tk.Tk()
         root.title(title)
-        label = tk.Label(root, text = msg)
-        label.pack(side = "top", fill = "x", pady=10)
+        label = tk.Label(root, text=msg)
+        label.pack(side="top", fill="x", pady=10)
 
-        B1 = tk.Button(root, text = "Okay", command = root.destroy)
+        B1 = tk.Button(root, text="Okay", command=root.destroy)
         B1.pack()
         root.mainloop()
 
